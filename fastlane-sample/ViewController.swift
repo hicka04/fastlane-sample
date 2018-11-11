ba56c26dae38f4809eaea91c0cd77e1513a223c6
hicka04 <calpissoda8221@gmail.com>
2018-11-11 23:28:47 +0900

    プロジェクト追加

---
+++ fastlane-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fastlane-sample
+//
+//  Created by hicka04 on 2018/11/10.
+//  Copyright © 2018 hicka04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
